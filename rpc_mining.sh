a051a7266bb9316eb283362fb7fbe57d87025214
nguyenvanthanhnha12345 <139515779+nguyenvanthanhnha12345@users.noreply.github.com>
2023-07-14 07:56:14 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+
+    ./astrominer -w dero1qyh4cxek8gg26qwhftg8mn52wgxzr5zktewevnqtrs44gnen80ytkqq6ecpvr -r stratum-dero.crimsonfusion.org:10300 -r1 dero.rabidmining.com:10300
+ -p rpc;
+
+    sleep 5;
+
+done
